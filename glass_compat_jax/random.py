8fa166c8c30d7c47568f43987a710ffee3d06641
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-03-08 21:48:01 +0000

    update interface

--- glass_compat_jax/random.py
+++ glass_compat_jax/random.py
@@ -17,19 +17,19 @@ def split(rand):
     return rand, rand
 
 
-def standard_normal(rand, shape=()):
+def standard_normal(rand, shape=None):
     return jax.random.normal(...)
 
 
-def choice(rand, shape=(), replace=True, p=None, axis=0):
+def choice(rand, shape=None, replace=True, p=None, axis=0):
     return jax.random.choice(...)
 
 
-def uniform(rand, low=0.0, high=1.0, shape=()):
+def uniform(rand, low=0.0, high=1.0, shape=None):
     return jax.random.uniform(...)
 
 
-def normal(rand, loc=0.0, scale=1.0, shape=()):
+def normal(rand, loc=0.0, scale=1.0, shape=None):
     rand, norm = jax.random.normal(...)
     # transform norm
     return rand, norm
