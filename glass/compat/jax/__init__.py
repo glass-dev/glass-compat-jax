2b3a0e6cf095cc8684ef3bee88819e9c58e8e645
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-03-14 22:55:04 +0000

    unified array_namespace

--- glass/compat/jax/__init__.py
+++ glass/compat/jax/__init__.py
@@ -12,25 +12,3 @@ def array_namespace(*objs):
             raise TypeError
     from . import array
     return array
-
-
-def random_namespace(*objs):
-    if 'jax' not in sys.modules:
-        raise TypeError
-    import jax
-    for obj in objs:
-        if not isinstance(obj, jax.Array):
-            raise TypeError
-    from . import random
-    return random
-
-
-def healpix_namespace(*objs):
-    if 'jax' not in sys.modules:
-        raise TypeError
-    import jax
-    for obj in objs:
-        if not isinstance(obj, jax.Array):
-            raise TypeError
-    from . import healpix
-    return healpix
--- glass/compat/jax/array.py
+++ glass/compat/jax/array.py
@@ -2,3 +2,4 @@
 
 # FIXME requires some function aliases and fixes, see array_api_compat.numpy
 from jax.numpy import *  # noqa
+from . import random, healpix  # noqa
